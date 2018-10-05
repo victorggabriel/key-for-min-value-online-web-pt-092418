aae44932e0dafc6baf2b3af1571adc70665b1f29
Victor Gonzalez <mywalletislost@gmail.com>
2018-10-05 19:43:26 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,7 +9,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if small > value || small == 0
         small = value
-        choosen_key = key 
+        choosen_key = key
     end
   end
   choosen_key
